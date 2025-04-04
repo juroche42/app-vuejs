1d292ecafd5de03795556bc1e0cd2bf5cd362b25
Jules Roche <julesro42@gmail.com>
2025-04-04 14:05:30 +0200

    feat: create new vieuw Product

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductView from '@/views/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
+    {
+      path: '/product',
+      name: 'product',
+      component: ProductView,
+    }
   ],
 })
 
---
+++ src/views/ProductView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import InfoSection from '@/components/InfoSection.vue'
+</script>
+
+<template>
+  <main class="max-w-6xl mx-auto px-4 py-10 grid md:grid-cols-2 gap-10">
+    <div>
+      <img src="/src/assets/logo.svg" class="w-full rounded-xl object-cover" alt="Produit">
+    </div>
+    <InfoSection/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
