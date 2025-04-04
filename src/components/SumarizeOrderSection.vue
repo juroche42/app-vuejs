9e6f3cd1d762dd8fa59f6fdf65e4555e327b2c04
Jules Roche <julesro42@gmail.com>
2025-04-04 14:55:54 +0200

    feat: function total

--- src/components/SumarizeOrderSection.vue
+++ src/components/SumarizeOrderSection.vue
@@ -1,14 +1,19 @@
 <script setup>
-
+const props = defineProps({
+  price: {
+    type: Number,
+    required: true,
+  },
+})
 </script>
 
 <template>
   <div class="text-right mt-10">
-    <p class="text-lg font-semibold">Total : <span class="text-blue-600">89,99 €</span></p>
-    <button class="mt-4 bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 transition">Commander</button>
+    <p class="text-lg font-semibold">Total : <span class="text-blue-600">{{props.price}} €</span></p>
+    <button class="mt-4 bg-blue-600 text-white px-6 py-3 rounded-xl hover:bg-blue-700 transition">
+      Commander
+    </button>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -2,14 +2,49 @@
 
 import SumarizeOrderSection from '@/components/SumarizeOrderSection.vue'
 import CheckoutElementCard from '@/components/CheckoutElementCard.vue'
+import { computed } from 'vue'
+
+const products = [
+  {
+    title: 'Chaussure',
+    price: 100,
+    quantity: 1,
+    image: '/src/assets/logo.svg',
+  },
+  {
+    title: 'Manteau',
+    price: 150,
+    quantity: 1,
+    image: '/src/assets/logo.svg',
+  },
+  {
+    title: 'Baskets',
+    price: 80,
+    quantity: 1,
+    image: '/src/assets/logo.svg',
+  },
+]
+
+let totalPrice = computed(() => {
+  let total = 0
+  products.forEach(product => {
+    total += product.price * product.quantity
+  })
+  return total
+})
 </script>
 
 <template>
   <main class="max-w-4xl mx-auto px-4 py-10">
     <h2 class="text-2xl font-bold mb-6">Mon Panier</h2>
     <div class="space-y-6">
-      <CheckoutElementCard title="Chaussure" price="100" />
-      <SumarizeOrderSection/>
+      <CheckoutElementCard v-for="product in products" :key="product.title"
+                           :title="product.title"
+                           :price="product.price"
+                           :quantity="product.quantity"
+                           :image="product.image"
+      />
+      <SumarizeOrderSection :price="totalPrice"/>
     </div>
   </main>
 </template>
