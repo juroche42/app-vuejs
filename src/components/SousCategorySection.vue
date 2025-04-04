e56f4148b53ad21d1347cc4431c40988c8ed921a
Jules Roche <julesro42@gmail.com>
2025-04-04 14:11:21 +0200

    init: create new component SousCategorySection

---
+++ src/components/SousCategorySection.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <a href="#" class="block border rounded-2xl overflow-hidden shadow hover:shadow-md transition">
+    <img src="/src/assets/logo.svg" alt="Ville" class="w-full h-48 object-cover">
+    <div class="p-4">
+      <h3 class="font-semibold text-lg">Chaussures de Ville</h3>
+      <p class="text-gray-600 text-sm">Classiques, élégantes pour vos journées de travail</p>
+    </div>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,5 +1,6 @@
 <script setup>
 
+import SousCategorySection from '@/components/SousCategorySection.vue'
 </script>
 
 <template>
@@ -7,31 +8,9 @@
     <h2 class="text-3xl font-bold mb-8 text-center">Chaussures Homme</h2>
 
     <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <!-- Sous-catégorie -->
-      <a href="#" class="block border rounded-2xl overflow-hidden shadow hover:shadow-md transition">
-        <img src="https://via.placeholder.com/400x250" alt="Ville" class="w-full h-48 object-cover">
-        <div class="p-4">
-          <h3 class="font-semibold text-lg">Chaussures de Ville</h3>
-          <p class="text-gray-600 text-sm">Classiques, élégantes pour vos journées de travail</p>
-        </div>
-      </a>
-
-      <a href="#" class="block border rounded-2xl overflow-hidden shadow hover:shadow-md transition">
-        <img src="https://via.placeholder.com/400x250" alt="Sport" class="w-full h-48 object-cover">
-        <div class="p-4">
-          <h3 class="font-semibold text-lg">Sneakers / Sport</h3>
-          <p class="text-gray-600 text-sm">Confort et style pour la ville ou la salle</p>
-        </div>
-      </a>
-
-      <a href="#" class="block border rounded-2xl overflow-hidden shadow hover:shadow-md transition">
-        <img src="https://via.placeholder.com/400x250" alt="Randonnée" class="w-full h-48 object-cover">
-        <div class="p-4">
-          <h3 class="font-semibold text-lg">Randonnée & Outdoor</h3>
-          <p class="text-gray-600 text-sm">Prêtes pour toutes vos aventures en plein air</p>
-        </div>
-      </a>
-      <!-- Ajoute plus de sous-catégories si besoin -->
+      <SousCategorySection/>
+      <SousCategorySection/>
+      <SousCategorySection/>
     </section>
   </main>
 </template>
