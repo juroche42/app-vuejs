1026194b48e99fc21f65e9023d2d407d75a71294
Jules Roche <julesro42@gmail.com>
2025-04-04 16:12:11 +0200

    feat: adding array

--- src/views/CategoryView.vue
+++ src/views/CategoryView.vue
@@ -1,6 +1,31 @@
 <script setup>
 
 import SousCategorySection from '@/components/SousCategorySection.vue'
+import { ref } from 'vue'
+
+const categories = ref([
+  {
+    title: 'Chaussures',
+    image: '/src/assets/logo.svg',
+    description: 'Découvrez notre sélection de chaussures pour homme.',
+  },
+  {
+    title: 'Vêtements',
+    image: '/src/assets/logo.svg',
+    description: 'Explorez notre gamme de vêtements pour homme.',
+  },
+  {
+    title: 'Accessoires',
+    image: '/src/assets/logo.svg',
+    description: 'Trouvez les accessoires parfaits pour compléter votre look.',
+  },
+  {
+    title: 'Bags',
+    image: '/src/assets/logo.svg',
+    description: 'Découvrez notre sélection de sacs pour homme.',
+  },
+])
+
 </script>
 
 <template>
@@ -8,8 +33,11 @@ import SousCategorySection from '@/components/SousCategorySection.vue'
     <h2 class="text-3xl font-bold mb-8 text-center">Chaussures Homme</h2>
 
     <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-      <SousCategorySection title="Chaussures"/>
-      <SousCategorySection title="Chaussures"/>
+      <SousCategorySection v-for="category in categories" :key="category.title"
+                           :title="category.title"
+                           :image="category.image"
+                           :description="category.description"
+      />
     </section>
   </main>
 </template>
